5f9ee072ec710e614c239c254c4b08452cb4ccd2
Eva Machova <evca.machova@gmail.com>
2022-05-21 14:06:10 +0200

    podrobnosti cesty

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,17 +1,14 @@
 import React, { useState } from 'react';
 import { JourneyPicker } from '../JourneyPicker';
+import { JourneyDetail } from '../JourneyDetail';
 
 export const Home = () => {
   const [journey, setJourney] = useState(null);
-
   console.log(journey);
-
   return (
     <main>
       <JourneyPicker onJourneyChange={setJourney} />
-      {journey === null ? null : (
-        <p>Nalezeno spojení s id {journey.journeyId}</p>
-      )}
+      {journey === null ? null : <JourneyDetail journey={journey} />}
     </main>
   );
 };
