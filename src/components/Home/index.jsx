d7c2fb039737226c2f94496bd51f1f6be9e6dc43
Eva Machova <evca.machova@gmail.com>
2022-05-21 15:14:00 +0200

    pridani rezervace

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,10 +2,32 @@ import React, { useState } from 'react';
 import { JourneyPicker } from '../JourneyPicker';
 import { JourneyDetail } from '../JourneyDetail';
 import { SelectedSeat } from '../SelectedSeat';
+import { useNavigate } from 'react-router-dom';
 
 export const Home = () => {
   const [journey, setJourney] = useState(null);
+  const navigate = useNavigate();
+
   console.log(journey);
+
+  const handleBuy = () => {
+    fetch('https://apps.kodim.cz/daweb/leviexpress/api/reservation', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        action: 'create',
+        seat: journey.autoSeat,
+        journeyId: journey.journeyId,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        navigate(`/reservation/${data.results.reservationId}`);
+      });
+  };
+
   return (
     <main>
       <JourneyPicker onJourneyChange={setJourney} />
@@ -13,6 +35,9 @@ export const Home = () => {
         <>
           <JourneyDetail journey={journey} />
           <SelectedSeat number={journey.autoSeat} />
+          <button className=" btn btn--big" onClick={handleBuy}>
+            Rezervovat
+          </button>
         </>
       )}
     </main>
--- src/components/Reservation/index.jsx
+++ src/components/Reservation/index.jsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './style.css';
+import { useParams } from 'react-router-dom';
 
 export const Reservation = () => {
-  return <h2>Detail jízdenky</h2>;
+  const { reservationId } = useParams();
+  const [reservation, setReservation] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      `https://apps.kodim.cz/daweb/leviexpress/api/reservation?id=${reservationId}`,
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setReservation(data.results);
+      });
+  }, []);
+
+  return (
+    <div className="reservation container">
+      <h2>Vaše e-jízdenka č. {reservationId}</h2>
+      <div className="reservation__body">
+        <div className="reservation__headings">
+          <p>Datum cesty:</p>
+          <p>Odjezd:</p>
+          <p>Příjezd:</p>
+          <p>Sedadlo:</p>
+        </div>
+        {reservation === null ? null : (
+          <div className="reservation__info">
+            <p>{reservation.date}</p>
+            <p>{reservation.fromCity.name}</p>
+            <p>{reservation.toCity.name}</p>
+            <p>{reservation.seatNumber}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
